f1e94b29e20a9c53f27749e607792bda1345a135
John Logan <john.logan@intel.com>
2019-06-21 11:25:31 +0100

    Added Additional Signing Keys
    Added example .bat file for signing OS and netloader.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>
    Signed-off-by: John Logan <john.logan@intel.com>

--- snr_secure_boot/CoSignedIFWIBuild/ibst_sign_all_OS_NL_internal.bat
+++ snr_secure_boot/CoSignedIFWIBuild/ibst_sign_all_OS_NL_internal.bat
@@ -6,28 +6,24 @@
 @REM Create bootx64.efi manifest
 @REM Create testImage manifest
 
-@REM INTEL CONFIDENTIAL
-@REM Copyright (c) 2016 - 2019, Intel Corporation. <BR>
+
+@REM Copyright (c) Intel Corporation 2016-2019. All rights reserved.
+@REM Permission is hereby granted, free of charge, to any person obtaining a copy
+@REM of this software and associated documentation files (the "Software"), to deal
+@REM in the Software without restriction, including without limitation the rights
+@REM to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+@REM copies of the Software, and to permit persons to whom the Software is
+@REM furnished to do so, subject to the following conditions:
+@REM The above copyright notice and this permission notice shall be included in
+@REM all copies or substantial portions of the Software.
 @REM
-@REM The source code contained or described herein and all documents related to the
-@REM source code ("Material") are owned by Intel Corporation or its suppliers or
-@REM licensors. Title to the Material remains with Intel Corporation or its suppliers
-@REM and licensors. The Material may contain trade secrets and proprietary    and
-@REM confidential information of Intel Corporation and its suppliers and licensors,
-@REM and is protected by worldwide copyright and trade secret laws and treaty
-@REM provisions. No part of the Material may be used, copied, reproduced, modified,
-@REM published, uploaded, posted, transmitted, distributed, or disclosed in any way
-@REM without Intel's prior express written permission.
-@REM
-@REM No license under any patent, copyright, trade secret or other intellectual
-@REM property right is granted to or conferred upon you by disclosure or delivery
-@REM of the Materials, either expressly, by implication, inducement, estoppel or
-@REM otherwise. Any license under such intellectual property rights must be
-@REM express and approved by Intel in writing.
-@REM
-@REM Unless otherwise agreed by Intel in writing, you may not remove or alter
-@REM this notice or any other notice embedded in Materials by Intel or
-@REM Intel's suppliers or licensors in any way.
+@REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+@REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+@REM FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+@REM AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+@REM LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+@REM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+@REM THE SOFTWARE.
 @REM
 
 
@@ -82,11 +78,11 @@ echo 		################# Signing ME Region Key manifest #################
 -s key="%KEY_PATH%/KAK0_3K_PRIVATE.pem"   ^
  rbe_bup_key="%KEY_PATH%/RBE_BUP_3K_PRIVATE.pem" ^
  pmc_key="%KEY_PATH%/PMCP_3K_PRIVATE.pem"   ^
- oem_debug_key="%KEY_PATH%/3k_test_key_private.pem" ^
- idlm_key="%KEY_PATH%/3k_test_key_private.pem" ^
+ oem_debug_key="%KEY_PATH%/TEST_KEY_3K_PRIVATE.pem" ^
+ idlm_key="%KEY_PATH%/TEST_KEY_3K_PRIVATE.pem" ^
  os_key="%KEY_PATH%/OS_3K_PRIVATE.pem" ^
  netl_key="%KEY_PATH%/NL_3K_PRIVATE.pem" ^
- fd0v_key="%KEY_PATH%/3k_test_key_private.pem" ^
+ fd0v_key="%KEY_PATH%/TEST_KEY_3K_PRIVATE.pem" ^
  output_name="%OUTPUT_PATH%/ME_KM_internal.bin" 
 
 echo 		###############  ME Region Key Manifest Done ####################
