6d5c4fe2e1f8d191f6ffab69e613b204c119ca20
John Jacques <john.jacques@intel.com>
2019-07-19 15:39:40 -0500

    Update the Data Logger Example
    Update to the latest releases, and add support for Linux
    4.19.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>

---
+++ linux/datalogger/datalog_reader/.tmp_versions/datalog_reader.mod
@@ -0,0 +1,3 @@
+/workspace/sw/jjacques/projects/axxia/axxia_support_private/linux/datalogger/datalog_reader/datalog_reader.ko
+/workspace/sw/jjacques/projects/axxia/axxia_support_private/linux/datalogger/datalog_reader/datalog_reader.o
+
--- linux/datalogger/datalog_reader/datalog_reader.c
+++ linux/datalogger/datalog_reader/datalog_reader.c
@@ -1,221 +1,211 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2003 INTEL Corporation
+
 /*
- * Axxia Pre-watchdog Datalog Reader
+ * Provides access to the "data log" saved when there is about to be
+ * a watchdog timeout.  Setting up the datalogger is described
+ * elsewhere.  As part of the datalogger setup, memory is reserved to
+ * store registers etc.  The address of this memory is in the device
+ * tree -- and this driver will read the device tree to determine the
+ * address.  The device tree entry should look like the following.
  *
- * Copyright (C) 2015 Intel Corporation
+ *       reserved-memory { #address-cells = <2>; #size-cells = <2>;
+ *       ranges;
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ *              datalogger_reserved: datalogger@30000000 { compatible =
+ *                       "datalogger"; reg = <0 0x30000000 0
+ *                       0x00020000>; no-map; };
+ *
+ *
+ *
+ *       };
+ *
+ * To recover the last values saved, do the following after the system
+ * reboots.
+ *
+ * -1-
+ * Create a device node in Linux /dev directory.
+ *
+ *     mknod /dev/datalogger c 60 0
+ *
+ * -2-
+ * Load the module created in this directory.
+ *
+ *     insmod datalog_reader.ko
+ *
+ * -3-
+ * Display the contents.
+ *
+ *     cat /dev/datalogger
  */
-
-/* Allows reading contents of data log stored in reserved memory block 'datalogger_reserved' */
-/* Device tree should include following definition (can change the addresses to suit your system */
-
-/*
-        reserved-memory {
-                #address-cells = <2>;
-                #size-cells = <2>;
-                ranges;
-
-               datalogger_reserved: datalogger@30000000 {
-                        compatible = "datalogger";
-                        reg = <0 0x30000000 0 0x00020000>;
-                        no-map;
-                };
-
- 
-
-        };
-
-
-
-Create a device node in Linux /dev directory using:
-
-mknod /dev/datalogger  c 60 0 
-
-
-To use datalogger, use cat command to read contents to a file:
-
-cat /dev/datalogger  > dump.txt
-
-or to screen
-
-cat /dev/datalogger
-
-
-
-*/
-
-
-
-
-#include "datalog_reader.h"
 
 #include <linux/ioport.h>
 #include <asm/io.h>
-
 #include <linux/of.h>
+#include <linux/uaccess.h>
 
+#include "datalog_reader.h"
 
+/* Global Variables */
 
-/* Global vars */
 void * data;
 int datalog_reader_major = 60;
 
 unsigned long long baseAddress = 0;
 unsigned long long int memorySize = 0;
 
-/* Structure that declares the usual file */
-/* access functions */
+/* File Access Function Structure */
+
 struct file_operations datalog_reader_fops = {
-  read: datalog_reader_read,
-  open: datalog_reader_open,
-  release: datalog_reader_release
+read: datalog_reader_read,
+open: datalog_reader_open,
+release: datalog_reader_release
 };
 
-/* Declaration of the init and exit functions */
-module_init(datalog_reader_init);
-module_exit(datalog_reader_exit);
+/*
+  ------------------------------------------------------------------------------
+  datalog_reader_open
+*/
 
-
-
-
-
-
-
-
-int datalog_reader_open(struct inode *inode, struct file *filp) 
+int
+datalog_reader_open(struct inode *inode, struct file *filp)
 {
-
-  //printk("< Open datalog_reader \n");
-  /* Success */
-  return 0;
+	return 0;
 }
 
-int datalog_reader_release(struct inode *inode, struct file *filp) 
+/*
+  ------------------------------------------------------------------------------
+  datalog_reader_release
+*/
+
+int
+datalog_reader_release(struct inode *inode, struct file *filp)
 {
- 
-   // printk("Close datalog_reader \n");
-  /* Success */
-  return 0;
+
+	return 0;
 }
 
+/*
+  ------------------------------------------------------------------------------
+  datalog_reader_read
+*/
 
+ssize_t
+datalog_reader_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
+{
 
-ssize_t datalog_reader_read(struct file *filp, char *buf, 
-                    size_t count, loff_t *f_pos) 
-{ 
- 
-  if (*f_pos >= memorySize)   /* If start position beyond end of datalog, return 0 bytes */
-  {
- 	return 0;
-  }
+	if (*f_pos >= memorySize)
+		/* If the start position is beyond the end of the data
+		 * log, return 0 bytes.
+		 */
+		return 0;
 
-  if ( (*f_pos + count) > memorySize) /* If request goes beyond end of datalog, truncate */
-  {
-  	count = memorySize - *f_pos;
-  }
-  
-   	if ( copy_to_user(buf, (data + *f_pos), count)  != 0)
-	{
+	if ((*f_pos + count) > memorySize)
+		/* If the request goes beyond the end of the data log,
+		 * truncate.
+		 */
+		count = memorySize - *f_pos;
+
+   	if (copy_to_user(buf, (data + *f_pos), count))
 		return -EFAULT;
+
+	*f_pos += count;
+
+	return count;
+}
+
+/*
+  ------------------------------------------------------------------------------
+  datalog_reader_init
+*/
+
+int
+datalog_reader_init(void)
+{
+	struct device_node *node;
+	struct device_node *dataloggerNode;
+	const unsigned long long * p;
+	unsigned int size;
+	int result;
+
+	/* Register Device */
+	result = register_chrdev(datalog_reader_major,
+				 "datalog_reader", &datalog_reader_fops);
+
+	if (0 > result) {
+		printk("Could not obtain major number %d!\n",
+		       datalog_reader_major);
+
+		return result;
 	}
 
-  *f_pos += count; 
+	/* Search for Data Log Memory Address in the Device Tree */
+	node = of_find_node_by_name(NULL, "reserved-memory");
+	if (!node) {
+		printk("Failed to find memory node in dts!\n");
+		datalog_reader_exit();
 
-  return count;
+		return 1;
+	}
 
+	dataloggerNode = of_get_child_by_name(node, "datalogger");
+	if (!dataloggerNode) {
+		printk("Failed to find datalogger_reserved node in dts!\n");
+		datalog_reader_exit();
+
+		return 1;
+	}
+
+	p = of_get_property(dataloggerNode, "reg", &size);
+	if (!p | (16 != size)) {
+		printk("Failed to find correctly formatted datalogger_reserved reg property in dts!\n");
+		datalog_reader_exit();
+
+		return 1;
+	}
+
+	baseAddress = be64_to_cpu(p[0]);
+	memorySize = be64_to_cpu(p[1]);
+
+	/* Request Data Log Memory */
+	if(!request_mem_region(baseAddress, memorySize, "datalog reader")) {
+		printk("Failed to get datalogger memory region!\n");
+		datalog_reader_exit();
+
+		return 1;
+	}
+
+	/* Get Data Log Memory */
+	data = ioremap(baseAddress, memorySize);
+	if (!data) {
+		printk("I/O remap failed!\n");
+		datalog_reader_exit();
+
+		return 1;
+	}
+
+	printk("Inserted datalog_reader Module\n");
+
+	return 0;
 }
 
+module_init(datalog_reader_init);
 
+/*
+  ------------------------------------------------------------------------------
+  datalog_reader_exit
+*/
 
-
-
-
-int datalog_reader_init(void) 
+void datalog_reader_exit(void)
 {
- 
- struct device_node *node;
- struct device_node *dataloggerNode;	
- const unsigned long long * p;
- unsigned int size;
+	/* Free the major number */
+	unregister_chrdev(datalog_reader_major, "datalog_reader");
 
- int result; 
-  
+	/* Free memory */
+	release_mem_region(baseAddress, memorySize);
+	iounmap(data);
 
-   /* Registering device */
-  result = register_chrdev(datalog_reader_major, "datalog_reader", &datalog_reader_fops);
-  if (result < 0) 
-  {
-    	printk("datalog_reader: cannot obtain major number %d\n", datalog_reader_major);
-    	return result;
-  }
- /* Search for datalogger memory area in dts */ 
-  node = of_find_node_by_name(NULL, "reserved-memory");
-  if (node ==NULL)
-  {
-	printk("Failed to find memory node in dts!\n");
-	datalog_reader_exit();
-	return 1;
-  }
-  
-  dataloggerNode = of_get_child_by_name(node, "datalogger");
-  if (dataloggerNode ==NULL)
-  {
-  	printk("Failed to find datalogger_reserved node in dts!\n");
-	datalog_reader_exit();
-	return 1;
-  }
-
-  p = of_get_property(dataloggerNode, "reg", &size);
-  if  ( (p == NULL) | (size !=16) )
- {
-	printk("Failed to find correctly formatted datalogger_reserved reg property in dts!\n");
-	datalog_reader_exit();
-	return 1;
-  }	
-	
-  baseAddress = be64_to_cpu(p[0]);
-  memorySize = be64_to_cpu(p[1]);
-	
-
- /* Request datalogger mem range */
-  if(request_mem_region(baseAddress, memorySize,  "datalog reader") == NULL)
-  {
-	printk("Failed to get datalogger memory region !!\n");
-	datalog_reader_exit();
-	return 1;
-  }
-
-   /* Get memory */
-   data = ioremap(baseAddress, memorySize);
-   if (data == NULL)
-   {
-	printk("I/O remap failed ! \n");
-	datalog_reader_exit();
-	return 1;
-   }
- 
-  printk("Inserting datalog_reader module\n"); 
-  return 0;
-
+	printk("Removed datalog_reader Module\n");
 }
 
-void datalog_reader_exit(void) 
-{
-/* Freeing the major number */
-
-  unregister_chrdev(datalog_reader_major, "datalog_reader");
-
-/* Free memory */
-  release_mem_region(baseAddress, memorySize);
-  iounmap(data);
-
-
-  printk("Removing datalog_reader \n");
-}
-
-
-
-
-	
+module_exit(datalog_reader_exit);
